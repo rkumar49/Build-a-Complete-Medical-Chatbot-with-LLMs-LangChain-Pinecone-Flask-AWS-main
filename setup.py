6c92fa8887231e23d2c1b06628ef9bf657f62ff2
rahul.kumar <rahul.kumar@st.ovgu.de>
2025-09-27 20:30:33 +0530

    app.py commited

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rahul Kumar",
+    author_email="rahulitssameer@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
